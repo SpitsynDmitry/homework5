6f1bc6ecb7a90bd4897c0df8505bdf937f11be27
Dmitry <d.spitsyn@internet.ru>
2022-05-26 12:37:22 +0300

    первый коммит дз задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        //задача 2.1
-        int[ ] weights = new int [12];
+        //задача 3.1
+        int[ ] weights = new int [3];
         weights[0] = 1;
         weights[1] = 2;
         weights[2] = 3;
 
-        for (int i = 0; i < 12; i++) {
-            System.out.print(weights[i] + " , ");
+        for (int i = 3-1; i >=0; i--){
+            if(weights[i]%2!=0){
+                weights [i] = weights [i] + 1;}
+            System.out.println(weights[i]);}
+        System.out.println("\n");
 
-        }
-        System.out.println(" ");
 
-         // задача 2.2
+        // задача 3.2
         double [ ] a = {1.57, 7.654, 9.986, 0, 0, 0, 0, 0, 0, 0, 0, 0,};
-        for (int i = 0; i < a.length; i++) {
-            if (i == a.length - 1) {
+        for (int i = a.length-1; i >=0; i--) {
+            if (i == 0) {
                 System.out.println(a[i]);
+                // Это последний член массива
             } else {
                 System.out.print(a[i] + " , ");
             }
         }
-        // задача 2.3
+        System.out.println("\n");
+        // задача 3.3
         int [ ] b = { 3, 50, 90, 0, 0, 0, 0, 0, 0, 12};
-        for (int i = 0; i < b.length; i++) {
-            System.out.print(b[i] + " , ");
+        for (int i = b.length -1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(b[i]);
+                // Это последний член массива
+            } else {
+                System.out.print(b[i] + " , ");
+            }
         }
         }
 
