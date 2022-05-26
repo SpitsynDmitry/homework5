8680d2601b881051a2c3f5ade4a2cd1334498d5c
Dmitry <d.spitsyn@internet.ru>
2022-05-26 08:21:14 +0300

    первый коммит дз задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        //задача 1.1
+        //задача 2.1
         int[ ] weights = new int [12];
         weights[0] = 1;
         weights[1] = 2;
         weights[2] = 3;
-        System.out.println(weights[0]);
-        System.out.println(weights[1]);
-        System.out.println(weights[2]);
-        System.out.println(weights[11]);
 
-         // задача 1.2
+        for (int i = 0; i < 12; i++) {
+            System.out.print(weights[i] + " , ");
+
+        }
+        System.out.println(" ");
+
+         // задача 2.2
         double [ ] a = {1.57, 7.654, 9.986, 0, 0, 0, 0, 0, 0, 0, 0, 0,};
-        System.out.println(a[0]);
-        System.out.println(a[1]);
-        System.out.println(a[2]);
-        System.out.println(a[11]);
-
-        // задача 1.3
+        for (int i = 0; i < a.length; i++) {
+            if (i == a.length - 1) {
+                System.out.println(a[i]);
+            } else {
+                System.out.print(a[i] + " , ");
+            }
+        }
+        // задача 2.3
         int [ ] b = { 3, 50, 90, 0, 0, 0, 0, 0, 0, 12};
-        System.out.println(b[0]);
-        System.out.println(b[1]);
-        System.out.println(b[2]);
-        System.out.println(b[9]);
-
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i] + " , ");
+        }
         }
 
 
